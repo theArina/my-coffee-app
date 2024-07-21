55361337fffd1e11398d63cc19d31a92a8690e4c
Arina M <thearinamarr@gmail.com>
2024-07-21 15:40:42 +0200

    Add loading dots on loading image

--- src/client/components/Card.svelte
+++ src/client/components/Card.svelte
@@ -1,26 +1,62 @@
 <script lang="ts">
   import '$styles/Card.css';
+  import LoadingDots from './LoadingDots.svelte';
+  import { onMount } from 'svelte';
+  import { getImage } from '$api';
 
   export let blendName: string;
   export let origin: string;
   export let variety: string;
   export let notes: string[];
   export let intensifier: string;
-  export let image: string = undefined;
-  export let imageStatus: string;
+  export let isFirstCardOnPage = false;
+
+  let image: string | undefined;
+  let imageStatus: 'loading' | 'success' | 'failure' = 'loading';
+
+  const getImageAlt = (): string => {
+    switch (imageStatus) {
+      case 'failure':
+        return `Failed to load "${blendName}" image :(`;
+      case 'success':
+        return `"${blendName}" image`;
+    }
+  };
+
+  async function loadImage() {
+    try {
+      const res = await getImage(isFirstCardOnPage);
+      image = res.file;
+      imageStatus = `success`;
+    } catch (e) {
+      console.error('Failed to fetch image data:', e);
+      imageStatus = 'failure';
+    }
+  }
+
+  onMount(() => {
+    loadImage();
+  });
 </script>
 
 <div class="card">
-  <div class="image-container">
-    <img src={image} alt={imageStatus} class="card-img" />
-    {#if image}
-      <div class="intensifier">{intensifier}</div>
-    {/if}
-  </div>
+  {#if imageStatus === 'loading'}
+    <LoadingDots />
+  {:else}
+    <div class="image-container">
+      <img src={image} alt={getImageAlt()} class="card-img" />
+      {#if image}
+        <div class="intensifier">{intensifier}</div>
+      {/if}
+    </div>
+  {/if}
   <div class="card-content">
     <h2>{blendName}</h2>
     <p><strong>Origin:</strong> {origin}</p>
     <p><strong>Variety:</strong> {variety}</p>
+    {#if imageStatus === 'failure'}
+      <p><strong>Intensifier:</strong> {intensifier}</p>
+    {/if}
     <div class="notes">
       {#each notes as note}
         <span class="note">{note}</span>
--- src/client/components/CoffeeList.svelte
+++ src/client/components/CoffeeList.svelte
@@ -4,7 +4,7 @@
   import Card from './Card.svelte';
   import Alert from './Alert.svelte';
   import LoadingDots from './LoadingDots.svelte';
-  import * as API from '$api';
+  import { getCoffee } from '$api';
 
   const INACTIVITY_TIME = 1000 * 30;
 
@@ -14,21 +14,20 @@
     variety: string;
     notes: string[];
     intensifier: string;
-    imageStatus: string;
-    image?: string;
+    isFirstCardOnPage: boolean;
   }
 
   let cards: CoffeeCard[] = [];
-  let loading = false;
+  let loadingCoffee = false;
   let isFirstCardOnPage = true;
   let timer: number;
   let error: string | null = null;
 
   async function loadCard() {
-    loading = true;
+    loadingCoffee = true;
     clearError();
     try {
-      const coffee = await API.getCoffee(isFirstCardOnPage);
+      const coffee = await getCoffee(isFirstCardOnPage);
       cards = [
         ...cards,
         {
@@ -37,29 +36,15 @@
           variety: coffee.variety,
           intensifier: coffee.intensifier,
           notes: coffee.notes.split(', '),
-          imageStatus: `Loading "${coffee.blend_name}" image...`,
+          isFirstCardOnPage,
         },
       ];
-      loadImage(cards.length - 1);
+      isFirstCardOnPage = false;
     } catch (e) {
       console.error('Failed to fetch coffee data:', e);
-      loading = false;
       error = e.message;
-    }
-  }
-
-  async function loadImage(cardIndex) {
-    try {
-      const image = await API.getImage(isFirstCardOnPage);
-      cards[cardIndex].image = image.file;
-      cards[cardIndex].imageStatus = `"${cards[cardIndex].blendName}" image`;
-    } catch (e) {
-      console.error('Failed to fetch image data:', e);
-      cards[cardIndex].imageStatus =
-        `Couldn't load "${cards[cardIndex].blendName}" image:(`;
     } finally {
-      loading = false;
-      isFirstCardOnPage = false;
+      loadingCoffee = false;
     }
   }
 
@@ -102,7 +87,7 @@
   {#each cards as card}
     <Card {...card} />
   {/each}
-  {#if loading || cards.length === 0}
+  {#if loadingCoffee || cards.length === 0}
     <LoadingDots />
   {:else}
     <button on:click={loadCard} class="load-more-button"> Load More</button>
